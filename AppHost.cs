b5e36d3c23c2acdbfec1f82f6f2217f9e71b8654
David Gardiner <david@gardinerfamily.au>
2025-11-07 14:51:06 +1030

    Convert to single-file Aspire app

--- AppHost.cs
+++ AppHost.cs
@@ -1,13 +1,16 @@
+#:sdk Aspire.AppHost.Sdk@13.1.0-preview.1.25556.11
+#:package Aspire.Hosting.JavaScript@13.1.0-preview.1.25556.11
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 
-builder.AddViteApp("vite-pnpm", "../vite-project")
+builder.AddViteApp("vite-pnpm", "vite-project")
     .WithPnpm()
     // Ensure the PATH includes the fnm (Fast Node Manager) default alias directory
     .WithEnvironment("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + Environment.ExpandEnvironmentVariables("%USERPROFILE%\\AppData\\Roaming\\fnm\\aliases\\default"))
     .WithExternalHttpEndpoints();
 
-builder.AddViteApp("vite-npm", "../vite-npm-project")
+builder.AddViteApp("vite-npm", "vite-npm-project")
     .WithEnvironment("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + Environment.ExpandEnvironmentVariables("%USERPROFILE%\\AppData\\Roaming\\fnm\\aliases\\default"))
     .WithExternalHttpEndpoints();
 
--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # aspire-vite-react
 
-Example Vite/React web app with .NET Aspire
+Example Vite/React web app with [Aspire](https://aspire.dev/)
 
 Includes two web apps, one using the `pnpm` package manager and the other using the `npm` package manager.
+
+## Prerequisites
+
+Requires .NET 10 SDK or later.
+
+Install the Aspire CLI by following the instructions at <https://aspire.dev/get-started/install-cli/>
+
+Then run
+
+```bash
+aspire run
+```
