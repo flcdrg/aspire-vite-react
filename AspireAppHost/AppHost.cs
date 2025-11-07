e3a3d6c4390b71b0406c6135ab306c7a97530841
David Gardiner <david@gardinerfamily.au>
2025-11-07 14:08:38 +1030

    Upgrade to Aspire 13.1 daily

--- AspireAppHost/AppHost.cs
+++ AspireAppHost/AppHost.cs
@@ -1,7 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddViteApp("viteapp", "../vite-project", "pnpm")
-    .WithEnvironment("PATH", (Environment.GetEnvironmentVariable("PATH") + ";" + Environment.ExpandEnvironmentVariables("%USERPROFILE%\\AppData\\Roaming\\fnm\\aliases\\default")))
-.WithExternalHttpEndpoints();
+builder.AddViteApp("viteapp", "../vite-project")
+    .WithPnpm()
+    .WithEnvironment("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + Environment.ExpandEnvironmentVariables("%USERPROFILE%\\AppData\\Roaming\\fnm\\aliases\\default"))
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
